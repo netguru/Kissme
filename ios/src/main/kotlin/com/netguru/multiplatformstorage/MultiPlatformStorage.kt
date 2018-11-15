1d1b95f57e1286291353f9097e9b6d27b87c3c9f
Sebastian Grabiński <sebastian.grabinski@netguru.pl>
2018-11-15 10:39:00 +0100

    Added CFDictionaryRef as main dictionary

--- ios/src/main/kotlin/com/netguru/multiplatformstorage/MultiPlatformStorage.kt
+++ ios/src/main/kotlin/com/netguru/multiplatformstorage/MultiPlatformStorage.kt
@@ -21,138 +21,140 @@ actual class MultiPlatformStorage {
 
     actual constructor(name: String?)
 
-    var accessGroup: NSString? = null
+    var accessGroup: CFStringRef? = null
         private set
 
     var serviceName: String = ""
-    private set
+        private set
 
     actual fun getAll(): Map<String, *> {
 
-        val keychainQueryMap: MutableMap<CFStringRef, Any?> = mutableMapOf()
+//        val keychainQueryMap: MutableMap<CFStringRef, Any?> = mutableMapOf()
+//
+//        keychainQueryMap[SecClass] = kSecClassGenericPassword
+//        keychainQueryMap[SecAttrService] = serviceName as CFStringRef
+//        keychainQueryMap[SecReturnAttributes] = kCFBooleanTrue
+//        keychainQueryMap[SecMatchLimit] = kSecMatchLimitAll
+//
+//        accessGroup?.let {
+//            keychainQueryMap[SecAttrAccessGroup] = accessGroup
+//        }
+//
+//        val result: CValuesRef<CFTypeRefVar>? = null
+//        val status = SecItemCopyMatching((keychainQueryMap as NSDictionary) as CFDictionaryRef, result)
+//
+//
+//        if (status != errSecSuccess) {
+//            return emptyMap<String, Any>()
+//        }
+//
+//        var values = mutableMapOf<String, Any>()
+//        val dictionaryResult = result as Array<NSDictionary>
+//
+//        for (attribute in dictionaryResult) {
+//            val accountData = attribute.valueForKey(SecAttrAccount as String) as String
+//            val value = attribute.valueForKey(SecValueData as String) as NSData
+//
+//            values[accountData] = (NSString.stringEncodingForData(data = value, encodingOptions = null, convertedString = null, usedLossyConversion = null )) as String
+//        }
 
-        keychainQueryMap[SecClass] = kSecClassGenericPassword
-        keychainQueryMap[SecAttrService] = serviceName as CFStringRef
-        keychainQueryMap[SecReturnAttributes] = kCFBooleanTrue
-        keychainQueryMap[SecMatchLimit] = kSecMatchLimitAll
-
-        accessGroup?.let {
-            keychainQueryMap[SecAttrAccessGroup] = accessGroup
-        }
-
-        val result: CValuesRef<CFTypeRefVar>? = null
-        val status = SecItemCopyMatching((keychainQueryMap as NSDictionary) as CFDictionaryRef, result)
-
-        if (status != errSecSuccess) {
-            return emptyMap<String, Any>()
-        }
-
-        var values = (mutableMapOf<String, Any>()) as  NSDictionary
-        val dictionaryResult = result as Array<NSDictionary>
-
-        for (attribute in dictionaryResult) {
-            val accountData = attribute.valueForKey(SecAttrAccount as String) as String
-            val value = attribute.valueForKey(SecValueData as String) as NSData
-
-            values[accountData] = (NSString.stringEncodingForData(data = value, encodingOptions = null, convertedString = null, usedLossyConversion = null )) as String
-        }
-
-        return values
+        return emptyMap<String,Any>()
     }
 
     actual fun getString(key: String, defaultValue: String?): String? {
-        data(key = key as NSString)?.let {
-            NSString.stringEncodingForData(data = it, encodingOptions = null, convertedString = null, usedLossyConversion = null)
-        }
-        return null
+//        data(key = key as NSString)?.let {
+//            NSString.stringEncodingForData(data = it, encodingOptions = null, convertedString = null, usedLossyConversion = null)
+//        }
+        return ""
     }
 
     actual fun putString(key: String, value: String) {
-        val key = key as NSString
-        val value = value as NSString
+        val key = key
+        val value = value
 
-        value.dataUsingEncoding(NSUTF8StringEncoding)?.let {
-            set(value = it, key = key)
-        }
+//        value.dataUsingEncoding(NSUTF8StringEncoding)?.let {
+            set(value = value, key = key)
+//        }
     }
-
+//
     actual fun getInt(key: String, defaultValue: Int): Int {
-        val key = key as NSString
-
-        val numberValue = objectForKey(key = key) as? NSNumber
-        numberValue?.let {
-            return it.intValue
-        }
+//        val key = key as NSString
+//
+//        val numberValue = objectForKey(key = key) as? NSNumber
+//        numberValue?.let {
+//            return it.intValue
+//        }
         return 0
     }
-
+//
     actual fun putInt(key: String, value: Int) {
-        set(value = NSNumber(value), key = key as NSString)
+//        set(value = NSNumber(value), key = key as NSString)
     }
-
+//
     actual fun getLong(key: String, defaultValue: Long): Long {
-        val key = key as NSString
-
-        val numberValue = objectForKey(key = key) as? NSNumber
-        numberValue?.let {
-            return it.longValue
-        }
+//        val key = key as NSString
+//
+//        val numberValue = objectForKey(key = key) as? NSNumber
+//        numberValue?.let {
+//            return it.longValue
+//        }
         return 0
     }
 
     actual fun putLong(key: String, value: Long) {
-        set(value = NSNumber(long = value), key = key as NSString)
+//        set(value = NSNumber(long = value), key = key as NSString)
     }
 
     actual fun getFloat(key: String, defaultValue: Float): Float {
-        val key = key as NSString
-
-        val numberValue = objectForKey(key = key) as? NSNumber
-        numberValue?.let {
-            return it.floatValue
-        }
+//        val key = key as NSString
+//
+//        val numberValue = objectForKey(key = key) as? NSNumber
+//        numberValue?.let {
+//            return it.floatValue
+//        }
         return Float.NaN
     }
 
     actual fun putFloat(key: String, value: Float) {
-        set(value = NSNumber(value), key = key as NSString)
+//        set(value = NSNumber(value), key = key as NSString)
     }
 
     actual fun getDouble(key: String, defaultValue: Double): Double {
-        val key = key as NSString
-
-        val numberValue = objectForKey(key = key) as? NSNumber
-        numberValue?.let {
-            return it.doubleValue
-        }
+//        val key = key as NSString
+//
+//        val numberValue = objectForKey(key = key) as? NSNumber
+//        numberValue?.let {
+//            return it.doubleValue
+//        }
         return 0.0
     }
 
     actual fun putDouble(key: String, value: Double) {
-        set(value = NSNumber(value), key = key as NSString)
+//        set(value = NSNumber(value), key = key as NSString)
     }
 
     actual fun getBoolean(key: String, defaultValue: Boolean): Boolean {
-        val key = key as NSString
-
-        val numberValue = objectForKey(key = key) as? NSNumber
-        numberValue?.let {
-            return it.boolValue
-        }
+//        val key = key as NSString
+//
+//        val numberValue = objectForKey(key = key) as? NSNumber
+//        numberValue?.let {
+//            return it.boolValue
+//        }
         return false
     }
 
     actual fun putBoolean(key: String, value: Boolean) {
-        set(value = value, key = key as NSString)
+//        set(value = value, key = key as NSString)
     }
 
     actual fun contains(key: String): Boolean {
-        val key = key as NSString
-        return data(key = key)?.let { true } ?: false
+//        val key = key as NSString
+//        return data(key = key)?.let { true } ?: false
+        return false
     }
 
     actual fun remove(key: String) {
-        removeObject(key = key as NSString)
+//        removeObject(key = key as NSString)
     }
 
     actual fun clear() {
@@ -170,78 +172,79 @@ actual class MultiPlatformStorage {
         return status == errSecSuccess
     }
 
-    private fun removeObject(key: NSString): Boolean {
-        val keychainQueryMap = setupKeychainQueryDictionary(forKey = key)
-        val status: OSStatus = SecItemDelete(query = keychainQueryMap as CFDictionaryRef)
+//    private fun removeObject(key: NSString): Boolean {
+//        val keychainQueryMap = setupKeychainQueryDictionary(forKey = key)
+//        val status: OSStatus = SecItemDelete(query = keychainQueryMap as CFDictionaryRef)
+//
+//        return status == errSecSuccess
+//    }
 
-        return status == errSecSuccess
+//    private fun set(value: Any, key: NSString): Boolean {
+//        val data = NSKeyedArchiver.archivedDataWithRootObject(value)
+//        return set(value = data, key = key)
+//    }
+//
+//    private fun set(value: Boolean, key: NSString): Boolean {
+//        return set(value = NSNumber(value), key = key)
+//    }
+//
+//    private fun set(value: NSString, key: NSString): Boolean {
+//        value.dataUsingEncoding(NSUTF8StringEncoding)?.let {
+//            set(value = it, key = key)
+//        }
+//        return false
+//    }
+
+    private fun set(value: String, key: String?): String {
+        val keychainQueryDictionary = setupKeychainQueryDictionary(forKey = key)
+
+        CFDictionaryAddValue(theDict = keychainQueryDictionary, key = SecValueData, value = value)
+        CFDictionaryAddValue(theDict = keychainQueryDictionary, key = SecValueData, value = value)
+
+        val status: OSStatus = SecItemAdd(keychainQueryDictionary, null)
+
+//        if (status == errSecSuccess) {
+//            return true
+//        } else if (status == errSecDuplicateItem) {
+//            print("Duplicate = need to update data")
+//        } else {
+//            return false
+//        }
+        return "${status}"
     }
 
-    private fun set(value: NSCodingProtocol, key: NSString): Boolean {
-        val data = NSKeyedArchiver.archivedDataWithRootObject(value)
-        return set(value = data, key = key)
-    }
+//    private fun data(key: String?): NSData? {
+//        val keychainQueryDictionary = setupKeychainQueryDictionary(forKey = key)
+//
+//        CFDictionaryAddValue(theDict = keychainQueryDictionary, key = SecMatchLimit, value = kSecMatchLimitOne)
+//        CFDictionaryAddValue(theDict = keychainQueryDictionary, key = SecReturnData, value = kCFBooleanTrue)
+//
+//        val result: CValuesRef<CFTypeRefVar>? = null
+//        val status = SecItemCopyMatching(keychainQueryDictionary, result)
+//
+//        return if (status == 0) result else null
+//    }
 
-    private fun set(value: Boolean, key: NSString): Boolean {
-        return set(value = NSNumber(value), key = key)
-    }
+//    private fun objectForKey(key: String?): NSCodingProtocol? = data(key = key)?.let {
+//        NSKeyedUnarchiver.unarchiveObjectWithData(data = it) as? NSCodingProtocol
+//    }
 
-    private fun set(value: NSString, key: NSString): Boolean {
-        value.dataUsingEncoding(NSUTF8StringEncoding)?.let {
-            set(value = it, key = key)
-        }
-        return false
-    }
+    private fun setupKeychainQueryDictionary(forKey: String?): CFMutableDictionaryRef? {
 
-    private fun set(value: NSData, key: NSString): Boolean {
-        val keychainQueryMap = setupKeychainQueryDictionary(forKey = key)
+        val keychainQueryDictionary = CFDictionaryCreateMutable(allocator = null, capacity = 0, keyCallBacks = null, valueCallBacks = null)
 
-        keychainQueryMap[SecValueData] = value
-        keychainQueryMap[SecAttrAccessible] = kSecAttrAccessibleWhenUnlocked
-
-        val status: OSStatus = SecItemAdd(keychainQueryMap as CFDictionaryRef, null)
-
-        if (status == errSecSuccess) {
-            return true
-        } else if (status == errSecDuplicateItem) {
-            print("Duplicate = need to update data")
-        } else {
-            return false
-        }
-        return false
-    }
-
-    private fun data(key: NSString): NSData? {
-        val keychainQueryMap = setupKeychainQueryDictionary(forKey = key)
-
-        keychainQueryMap[SecMatchLimit] = kSecMatchLimitOne
-        keychainQueryMap[SecReturnData] = kCFBooleanTrue
-
-        val result: CValuesRef<CFTypeRefVar>? = null
-        val status = SecItemCopyMatching(keychainQueryMap as CFDictionaryRef, result)
-
-        return if (status == 0) result as? NSData else null
-    }
-
-    private fun objectForKey(key: NSString): NSCodingProtocol? = data(key = key)?.let {
-        NSKeyedUnarchiver.unarchiveObjectWithData(data = it) as? NSCodingProtocol
-    }
-
-    private fun setupKeychainQueryDictionary(forKey: NSString): MutableMap<CFStringRef, Any?> {
-        val keychainQueryMap: MutableMap<CFStringRef, Any?> = mutableMapOf()
-
-        keychainQueryMap[this.SecClass] = kSecClass
-        keychainQueryMap[this.SecAttrService] = kSecAttrService
+        CFDictionaryAddValue(theDict = keychainQueryDictionary, key = SecClass, value = kSecClass)
+        CFDictionaryAddValue(theDict = keychainQueryDictionary, key = SecAttrService, value = kSecAttrService)
 
         accessGroup?.let {
-            keychainQueryMap[this.SecAttrAccessGroup] = it
+            CFDictionaryAddValue(theDict = keychainQueryDictionary, key = SecAttrAccessGroup, value = it)
         }
 
-        val encodedIdentifier = forKey.dataUsingEncoding(NSUTF8StringEncoding)
+        val encodedIdentifier = CFStringCreateWithCString(null, cStr = forKey, encoding = kCFStringEncodingMacRoman)
 
-        keychainQueryMap[this.SecAttrGeneric] = encodedIdentifier
-        keychainQueryMap[this.SecAttrAccount] = encodedIdentifier
+        CFDictionaryAddValue(theDict = keychainQueryDictionary, key = SecAttrGeneric, value = encodedIdentifier)
+        CFDictionaryAddValue(theDict = keychainQueryDictionary, key = SecAttrAccount, value = encodedIdentifier)
 
-        return keychainQueryMap
+        return keychainQueryDictionary
     }
 }
