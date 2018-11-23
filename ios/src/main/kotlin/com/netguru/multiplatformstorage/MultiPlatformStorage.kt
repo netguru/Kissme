37f652e8c4dd6d77cd916c1a3e3cc9267934eba9
Sebastian Grabiński <sebastian.grabinski@netguru.pl>
2018-11-23 13:26:33 +0100

    Keychain framework test calling methods

--- ios/src/main/kotlin/com/netguru/multiplatformstorage/MultiPlatformStorage.kt
+++ ios/src/main/kotlin/com/netguru/multiplatformstorage/MultiPlatformStorage.kt
@@ -1,56 +1,76 @@
 package com.netguru.multiplatformstorage
-
 import com.netguru.keychainWrapper.*
 
 actual class MultiPlatformStorage actual constructor(name: String?) {
 
     actual fun getAll(): Map<String, *> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return mapOf<String, String>()
     }
 
     actual fun getString(key: String, defaultValue: String?): String? {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return ""
     }
 
-    actual fun putString(key: String, value: String) {}
+    actual fun putString(key: String, value: String) {
+        Keychain.setPassword(password="ok", forService= "ok12", account= "ok123")
+
+//        Keychain.setPassword(password = "ok", forService= "ok12", account= "ok123")
+//        val accounts = Keychain.accountsForService(serviceName= "ok12")
+//        print(accounts)
+    }
 
     actual fun getInt(key: String, defaultValue: Int): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return 0
     }
 
-    actual fun putInt(key: String, value: Int) {}
+    actual fun putInt(key: String, value: Int) {
+        print("ok2")
+    }
 
     actual fun getLong(key: String, defaultValue: Long): Long {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return 0
     }
 
-    actual fun putLong(key: String, value: Long) {}
+    actual fun putLong(key: String, value: Long) {
+        print("ok3")
+    }
 
     actual fun getFloat(key: String, defaultValue: Float): Float {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return 1.5.toFloat()
     }
 
-    actual fun putFloat(key: String, value: Float) {}
+    actual fun putFloat(key: String, value: Float) {
+        print("ok4")
+    }
 
     actual fun getDouble(key: String, defaultValue: Double): Double {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return 0.0
     }
 
-    actual fun putDouble(key: String, value: Double) {}
+    actual fun putDouble(key: String, value: Double) {
+        print("ok5")
+    }
 
     actual fun getBoolean(key: String, defaultValue: Boolean): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return false
     }
 
-    actual fun putBoolean(key: String, value: Boolean) {}
+    actual fun putBoolean(key: String, value: Boolean) {
+        print("ok6")
+
+    }
 
     actual fun contains(key: String): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return true
     }
 
-    actual fun remove(key: String) {}
+    actual fun remove(key: String) {
+        print("ok7")
+
+    }
 
     actual fun clear() {
+        print("ok8")
 
     }
 }
