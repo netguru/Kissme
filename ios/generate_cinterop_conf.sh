11d75e3b7d79f5d8b7a7b8e6c2a85c648d767a6c
Samuel Urbanowicz <samuel.urbanowicz@netguru.pl>
2018-11-30 12:21:17 +0100

    Tune up .def file generation script

--- ios/generate_cinterop_conf.sh
+++ ios/generate_cinterop_conf.sh
@@ -1,13 +1,16 @@
+#! /bin/bash
 current_dir=`pwd`
-target_file_path="./src/main/c_interop/keychainwrapper.def"
+target_file_dir="./src/main/c_interop/"
+target_file_name="keychainwrapper.def"
 
-rm -f $target_file_path
+rm -f "$target_file_dir$target_file_name"
 
-echo "writing config to ./src/main/c_interop/keychainwrapper.def"
+echo "writing config to $target_file_dir$target_file_name"
 
+mkdir -p $target_file_dir
 echo \
 "language = Objective-C
 headers = Keychain.h KeychainQuery.h KeychainWrapper.h
 compilerOpts = -framework keychainwrapper
 linkerOpts = -F$current_dir -framework keychainwrapper" \
->> $target_file_path
+>> "$target_file_dir$target_file_name"
