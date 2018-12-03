997fee622af39d237b64c519d9e79ac514f46a69
Sebastian Grabiński <sebastian.grabinski@netguru.pl>
2018-12-03 21:00:13 +0100

    Added serviceName property

--- ios/src/main/kotlin/com/netguru/multiplatformstorage/MultiPlatformStorage.kt
+++ ios/src/main/kotlin/com/netguru/multiplatformstorage/MultiPlatformStorage.kt
@@ -4,6 +4,8 @@ import platform.Foundation.*
 
 actual class MultiPlatformStorage actual constructor(name: String?) {
 
+    private val serviceName: String = name ?: "defaultservice"
+
     actual fun getAll(): Map<String, *> {
         val allPasswords = Keychain.getAllPasswordForService("defaultservice") ?: emptyMap<String, String>()
         return allPasswords
@@ -15,59 +17,59 @@ actual class MultiPlatformStorage actual constructor(name: String?) {
     }
 
     actual fun getString(key: String, defaultValue: String?): String? {
-        return Keychain.passwordForService("defaultservice", account=key)
+        return Keychain.passwordForService(serviceName, account=key)
     }
 
     actual fun putString(key: String, value: String) {
-        Keychain.setPassword(value, "defaultservice", account= key)
+        Keychain.setPassword(value, serviceName, account= key)
     }
 
     actual fun getInt(key: String, defaultValue: Int): Int {
-        return Keychain.passwordForService(serviceName="defaltservice", account=key)?.toInt() ?: defaultValue
+        return Keychain.passwordForService(serviceName=serviceName, account=key)?.toInt() ?: defaultValue
     }
 
     actual fun putInt(key: String, value: Int) {
-        Keychain.setPassword(value.toString(), "defaultservice" , account= key)
+        Keychain.setPassword(value.toString(), serviceName , account= key)
     }
 
     actual fun getLong(key: String, defaultValue: Long): Long {
-        return Keychain.passwordForService(serviceName="defaltservice", account=key)?.toLong() ?: defaultValue
+        return Keychain.passwordForService(serviceName=serviceName, account=key)?.toLong() ?: defaultValue
     }
 
     actual fun putLong(key: String, value: Long) {
-        Keychain.setPassword(value.toString(),"defaultservice" , account= key)
+        Keychain.setPassword(value.toString(),serviceName , account= key)
     }
 
     actual fun getFloat(key: String, defaultValue: Float): Float {
-        return Keychain.passwordForService(serviceName="defaltservice", account=key)?.toFloat() ?: defaultValue
+        return Keychain.passwordForService(serviceName=serviceName, account=key)?.toFloat() ?: defaultValue
     }
 
     actual fun putFloat(key: String, value: Float) {
-        Keychain.setPassword(value.toString(), "defaultservice" , key)
+        Keychain.setPassword(value.toString(), serviceName , key)
     }
 
     actual fun getDouble(key: String, defaultValue: Double): Double {
-        return Keychain.passwordForService(serviceName="defaltservice", account=key)?.toDouble() ?: defaultValue
+        return Keychain.passwordForService(serviceName=serviceName, account=key)?.toDouble() ?: defaultValue
     }
 
     actual fun putDouble(key: String, value: Double) {
-        Keychain.setPassword(value.toString(), "defaultservice", key)
+        Keychain.setPassword(value.toString(), serviceName, key)
     }
 
     actual fun getBoolean(key: String, defaultValue: Boolean): Boolean {
-        return Keychain.passwordForService(serviceName="defaltservice", account=key)?.toBoolean() ?: defaultValue
+        return Keychain.passwordForService(serviceName=serviceName, account=key)?.toBoolean() ?: defaultValue
     }
 
     actual fun putBoolean(key: String, value: Boolean) {
-        Keychain.setPassword(value.toString(), "defaultservice", key)
+        Keychain.setPassword(value.toString(), serviceName, key)
     }
 
     actual fun contains(key: String): Boolean {
-        return Keychain.containsForService("defaultservice",key)
+        return Keychain.containsForService(serviceName,key)
     }
 
     actual fun remove(key: String) {
-        Keychain.deletePasswordForService("defaultservice", key)
+        Keychain.deletePasswordForService(serviceName, key)
     }
 
     actual fun clear() {
