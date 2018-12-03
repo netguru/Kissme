e06ebce377ee6a5d5658dc286ceda0203090baaf
Samuel Urbanowicz <samuel.urbanowicz@netguru.pl>
2018-12-03 14:23:43 +0100

    Use static .a library

--- ios/generate_cinterop_conf.sh
+++ ios/generate_cinterop_conf.sh
@@ -10,7 +10,9 @@ echo "writing config to $target_file_dir$target_file_name"
 mkdir -p $target_file_dir
 echo \
 "language = Objective-C
-headers = Keychain.h KeychainQuery.h KeychainWrapper.h
-compilerOpts = -framework keychainwrapper
-linkerOpts = -F$current_dir -framework keychainwrapper" \
+headers = Keychain.h KeychainQuery.h
+compilerOpts = -F$current_dir
+linkerOpts = -F$current_dir/library
+staticLibraries = keychainwrapper.a
+libraryPaths = $current_dir/library" \
 >> "$target_file_dir$target_file_name"
